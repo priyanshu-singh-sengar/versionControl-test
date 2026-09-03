9cad1524b5ef55fd6e2ec4bd37a2586fc47f497e
Priyanshu Singh <zxenusanger@gmail.com>
2026-09-03 15:28:19 +0530

    feat: add starter JS entrypoint for CodeQL analysis

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ * VersionControl-Test Application Entrypoint
+ */
+
+function getAppInfo() {
+  return {
+    name: "versionControl-test",
+    version: "1.0.0",
+    description: "AZ-400 Version Control & Security Lab"
+  };
+}
+
+module.exports = { getAppInfo };
